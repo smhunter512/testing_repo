28318f295510974e1b27907cb37512cc399bf40d
Scott Hunter <101737058+smhunter512@users.noreply.github.com>
2022-04-09 13:42:52 -0400

    new stuff

--- script01.py
+++ script01.py
@@ -1,1 +1,16 @@
 #Test 01
+
+# Write a function that returns the arithmetic average for a list of numbers
+def average(numbers):
+    length = len(numbers)
+    total = 0
+    for number in numbers:
+        total += number
+    return total / length
+	
+
+
+# Test your function with the following:
+print(average([1, 5, 10]))
+print(average(range(11)))
+
